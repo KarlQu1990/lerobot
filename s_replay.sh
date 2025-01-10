87c606a50e10e809545783e5718913a46d4941c7
karl <304624555@qq.com>
2025-01-10 10:11:56 +0800

    update

--- s_replay.sh
+++ s_replay.sh
@@ -6,10 +6,12 @@ fi
 
 DATA_ROOT=/home/hhws/projects/robot_datasets
 DATASET_NAME=$HF_USER/so100_test1
+ROBOT_PATH=lerobot/configs/robot/so100.yaml
 EPISODE=0
 FPS=30
 
-python lerobot/scripts/control_robot.py teleoperate \
+python lerobot/scripts/control_robot.py replay \
+    --robot-path $ROBOT_PATH \
     --fps $FPS \
     --root $DATA_ROOT \
     --episode $EPISODE \
