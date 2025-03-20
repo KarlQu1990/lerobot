4757d606dae01bd7aeffd091adcc4bb4fe93798b
qukaiyi <304624555@qq.com>
2025-03-20 15:19:57 +0800

    update

--- s_train.sh
+++ s_train.sh
@@ -6,19 +6,20 @@ if [ "$HF_USER" = "Not logged in" ];then
 fi
 
 DATA_ROOT=/home/hhws/projects/robot_datasets
-DATASET_NAMES=$HF_USER/so100_bimanual_clothes_5
-POLICY_NAME=act
-OUTPUT_DIR=outputs/train/act_so100_bimanual_clothes_6
-JOB_NAME=act_so100_bimanual_clothes_6
+#DATASET_NAMES=$HF_USER/so100_bimanual_clothes_5
+DATASET_NAMES=$HF_USER/so100_bimanual_transfer_6
+POLICY_NAME=pi0
+#POLICY_NAME=act
+OUTPUT_DIR=outputs/train/pi0_so100_bimanual_transfer_1
+JOB_NAME=pi0_so100_bimanual_clothes_6
 DEVICE=cuda
-STEPS=300000
-SAVE_FREQ=20000
+STEPS=20000
+SAVE_FREQ=5000
 BATCH_SIZE=8
 ENABLE_IMAGE_TRANSFORM=false
 RESUME=false
-USE_AMP=false
 # PRETRAINED_PATH=outputs/train/act_so100_bimanual_transfer_5/checkpoints/last/pretrained_model
-PRETRAINED_PATH=
+PRETRAINED_PATH=lerobot/pi0
 
 if [ -z $PRETRAINED_PATH ]; then
   if [ $RESUME = true ]; then
@@ -28,8 +29,7 @@ if [ -z $PRETRAINED_PATH ]; then
     --config_path=$OUTPUT_DIR/checkpoints/last/pretrained_model/train_config.json \
     --batch_size=$BATCH_SIZE \
     --steps=$STEPS \
-    --save_freq=$SAVE_FREQ \
-    --use_amp=$USE_AMP 
+    --save_freq=$SAVE_FREQ
   else
     echo "从头开始训练。"
     python lerobot/scripts/train.py \
@@ -40,7 +40,6 @@ if [ -z $PRETRAINED_PATH ]; then
       --output_dir=$OUTPUT_DIR \
       --job_name=$JOB_NAME \
       --device=$DEVICE \
-      --use_amp=$USE_AMP \
       --batch_size=$BATCH_SIZE \
       --steps=$STEPS \
       --save_freq=$SAVE_FREQ
@@ -55,7 +54,6 @@ else
     --output_dir=$OUTPUT_DIR \
     --job_name=$JOB_NAME \
     --device=$DEVICE \
-    --use_amp=$USE_AMP \
     --batch_size=$BATCH_SIZE \
     --steps=$STEPS \
     --save_freq=$SAVE_FREQ
